7fb525f467f8662bbe059cdddd67768740e236b9
Bryan <bryan@bryanle.io>
2017-08-15 23:19:40 -0400

    Express application displays test object on port 5000

--- server/index.js
+++ server/index.js
@@ -1,2 +1,8 @@
 const express = require('express'); // Common JS convention for back end; different from ES6 'require' in front end
-const app = express(); // Generates single Express application; most applications will contain 1 application, some can contain more
+const app = express(); // Generates single Express application; most applications will contain 1 application, some can contain more. App listens to incoming requests that are being routed to expres side from node side
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+app.listen(5000);
