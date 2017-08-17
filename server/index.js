654170659aa2194d2359d7eee4f0a9eee3379aa3
Bryan <bryan@bryanle.io>
2017-08-16 22:17:35 -0400

    Google OAuth connection on localhost:5000/auth/google leads to Google with error

--- server/index.js
+++ server/index.js
@@ -8,13 +8,20 @@ const app = express(); // Generates single Express application; most application
 
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback' // User returning from Google
-}, (accessToken) => {
-    console.log(accessToken);
-})
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback' // User returning from Google
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
 ); // Passport becoming aware of new strategy (Google). Creates instance of Google authentication
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', { // GoogleStrategy is internally known as 'google', therefore passport will know to refer to GoogleStrategy when the string 'google' is applied
+        scope: ['profile', 'email'] // Specifies to Google server what access we are requesting 
+    })
+)
 
 const PORT = process.env.PORT || 5000; // Use Heroku assigned port for production and port 5000 for development
 app.listen(process.env.PORT || 5000);
