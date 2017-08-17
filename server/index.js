6694ef4a6522da31559a8b458421a216860e1bd8
Bryan <bryan@bryanle.io>
2017-08-16 22:42:58 -0400

    Successfully receiving user data from Google OAuth

--- server/index.js
+++ server/index.js
@@ -11,8 +11,11 @@ passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback' // User returning from Google
-    }, (accessToken) => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+        
     })
 ); // Passport becoming aware of new strategy (Google). Creates instance of Google authentication
 
