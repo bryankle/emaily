4a5eb84157d7a042288a9fffc2638274f8aa27ce
Bryan <bryan@bryanle.io>
2017-08-16 20:54:27 -0400

    Removed test code and installed passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require('express'); // Common JS convention for back end; different from ES6 'require' in front end
 const app = express(); // Generates single Express application; most applications will contain 1 application, some can contain more. App listens to incoming requests that are being routed to expres side from node side
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
 
 const PORT = process.env.PORT || 5000; // Use Heroku assigned port for production and port 5000 for development
 app.listen(process.env.PORT || 5000);
