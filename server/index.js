79f4725b1445cf81a8011bd2bd9be503427f2247
Bryan <bryan@bryanle.io>
2017-08-16 22:00:26 -0400

    Implementing Google+ API Oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express'); // Common JS convention for back end; different from ES6 'require' in front end
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport'); // General passport code
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Passport specific to Google
 
 const app = express(); // Generates single Express application; most applications will contain 1 application, some can contain more. App listens to incoming requests that are being routed to expres side from node side
 
+
+passport.use(new GoogleStrategy()); // Passport becoming aware of new strategy (Google). Creates instance of Google authentication
 
 const PORT = process.env.PORT || 5000; // Use Heroku assigned port for production and port 5000 for development
 app.listen(process.env.PORT || 5000);
