4fd911e47856807cfbb8631ef2e30a9514ba0b24
Bryan <bryan@bryanle.io>
2017-08-22 23:52:58 -0400

    Importing mongoose into passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,18 @@
 const passport = require('passport'); // General passport code
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // Passport specific to Google
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback' // User returning from Google
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile:', profile);
+    // console.log('accessToken', accessToken);
+    // console.log('refresh token', refreshToken);
+    // console.log('profile:', profile);
     
 })
 ); // Passport becoming aware of new strategy (Google). Creates instance of Google authentication
