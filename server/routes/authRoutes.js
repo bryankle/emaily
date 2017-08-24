0b39d825cf43d0929363185a5278e720287659d9
Bryan <bryan@bryanle.io>
2017-08-24 19:18:51 -0400

    Logout feature implemented

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,6 +11,11 @@ module.exports = (app) => {
     
     app.get('/auth/google/callback', passport.authenticate('google')) // Route after user authenticates with Google OAuth
 
+    app.get('/api/logout', (req, res) => { // Whenever user makes a request to this directory, log user out
+        req.logout(); // logout is a function automatically attached to the req object by passport. When called, ogout takes cookie that contains our user ID and kills it
+        res.send(req.user); // Proves to user they are not signed in; returns 'nothing' or undefined
+    })
+
     app.get('/api/current_user', (req, res) => { // When someone makes a request to application, req is incoming, res is outgoing. Veifies someone who has already gone through  OAuth flow can now access user
         res.send(req.user);
     });
