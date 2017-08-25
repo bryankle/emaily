7ec604e065ff88e6f9ec35a7ff915f0a8fd4ac58
Bryan <bryan@bryanle.io>
2017-08-24 21:39:27 -0400

    Setting up config separation for dev and prod

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this!!!
+module.exports = {
+    googleClientID: '621101451589-s9oq4nejr8b00hrskfulgl1vf7oe9c7h.apps.googleusercontent.com',
+    googleClientSecret: 'GT4Gnkyu1mkG9EN5EJcktNEA',
+    mongoURI: 'mongodb://bryankle:Bkl17520@ds149603.mlab.com:49603/emaily-dev-bryankle',
+    cookieKey: 'ifojewafjajfkdlsafjfaewiofjadskl'
+}
