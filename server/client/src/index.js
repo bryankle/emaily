7a7c7c7ff6411a46bce8ab58e555c8a9c9296d56
Bryan <bryan@bryanle.io>
2017-08-28 23:43:32 -0400

    Redux boilerplate setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root')); // Takes 2 parameters, route component and where the component is going to be rendered
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider stor={store}><App /></Provider>, 
+    document.querySelector('#root')
+); // Takes 2 parameters, route component and where the component is going to be rendered
